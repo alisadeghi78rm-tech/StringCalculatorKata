8b9f02a78d3fd992b934735b88e1a561e93086db
Sadeghi <alisadeghi78rm@gmail.com>
2026-06-12 09:50:19 +0200

    feat: implement findSpaceship logic with coordinate calculation

--- src/help_alf.js
+++ src/help_alf.js
@@ -4,4 +4,11 @@ HelpAlf.prototype.findSpaceship = function(map) {
   if (map.indexOf('X') === -1) {
     return "Spaceship lost forever.";
   }
+  var lines = map.split('\n').reverse(); // Drehen, da 0,0 unten ist
+  for (var y = 0; y < lines.length; y++) {
+    var x = lines[y].indexOf('X');
+    if (x !== -1) {
+      return [x, y];
+    }
+  }
 };
