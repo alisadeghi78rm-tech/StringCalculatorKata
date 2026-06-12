6acec28ccf76f681651d710a630e328c535e36f7
Sadeghi <alisadeghi78rm@gmail.com>
2026-06-12 09:26:47 +0200

    feat: implement sum logic for comma-separated numbers

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -4,5 +4,10 @@ StringCalculator.prototype.add = function(string_numbers) {
 	if(string_numbers === ""){
 		return 0;
 	}
-	return parseInt(string_numbers);
+	var numbers = string_numbers.split(",");
+  	var sum = 0;
+  		for (var i = 0; i < numbers.length; i++) {
+    	sum += parseInt(numbers[i]);
+  		}
+  	return sum;
 };
