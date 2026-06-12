5940ebef9364126f45237921effe7df844407787
Sadeghi <alisadeghi78rm@gmail.com>
2026-06-12 09:28:32 +0200

    test: add test case for multi-digit numbers

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -12,4 +12,8 @@ describe("StringCalculator", function() {
   it("should return the number when a single number is passed", function() {
     expect(calculator.add("1")).toEqual(1);
   });
+
+  it("should return the sum of two numbers separated by a comma", function() {
+    expect(calculator.add("1,2")).toEqual(3);
+  });
 });
