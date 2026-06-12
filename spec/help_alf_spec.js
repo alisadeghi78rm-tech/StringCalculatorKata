80592fe059db0d9fae60b9d5a758e4b71c60efa6
Sadeghi <alisadeghi78rm@gmail.com>
2026-06-12 09:51:35 +0200

    test: add test case for example map with [7, 2] coordinates

--- spec/help_alf_spec.js
+++ spec/help_alf_spec.js
@@ -15,6 +15,11 @@ describe("HelpAlf", function() {
     it("should return 'Spaceship lost forever.' if the spaceship 'X' is not in the map", function() {
     var emptyMap = "....\n....";
     expect(alf.findSpaceship(emptyMap)).toEqual("Spaceship lost forever.");
-  });
+    });
+
+    it("should return [0, 0] if 'X' is at the bottom left", function() {
+    var map = "....\n....\nX..."; 
+    expect(alf.findSpaceship(map)).toEqual([0, 0]);
+    });
 
 });
