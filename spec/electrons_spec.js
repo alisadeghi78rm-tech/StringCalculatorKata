519d291461726a71ce1438cb91d2675d74bc453c
Sadeghi <alisadeghi78rm@gmail.com>
2026-06-12 10:14:27 +0200

    test: add second test case for electrons

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -15,4 +15,8 @@ describe("Electrons", function() {
     it("should return 6 for [1, 2, 3, 4, 5]", function() {
     expect(game.calculate([1, 2, 3, 4, 5])).toEqual(6);
     });
+
+    it("should return 4 for [2, 2, 3, 3]", function() {
+    expect(game.calculate([2, 2, 3, 3])).toEqual(4);
+    });
 });
