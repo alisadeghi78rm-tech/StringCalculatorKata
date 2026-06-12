7ff900df84cb95c882ed16d1cfdefdc85abb26d2
Sadeghi <alisadeghi78rm@gmail.com>
2026-06-12 10:16:14 +0200

    test: add remaining test cases for Electrons kata

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -19,4 +19,12 @@ describe("Electrons", function() {
     it("should return 4 for [2, 2, 3, 3]", function() {
     expect(game.calculate([2, 2, 3, 3])).toEqual(4);
     });
+
+    it("should return 2 for [6, 6, 4, 4, 1, 3]", function() {
+    expect(game.calculate([6, 6, 4, 4, 1, 3])).toEqual(2);
+    });
+
+    it("should return 12 for [3, 5, 3, 5, 4, 2]", function() {
+    expect(game.calculate([3, 5, 3, 5, 4, 2])).toEqual(12);
+    });
 });
