8dde7e01079fa6a74878645a34c1f1234e6ee412
Sadeghi <alisadeghi78rm@gmail.com>
2026-06-12 09:25:22 +0200

    test: add test case for two comma-separated numbers

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -8,4 +8,8 @@ describe("StringCalculator", function() {
   it("should return 0 for an empty string", function() {
     expect(calculator.add("")).toEqual(0);
   });
+
+  it("should return the number when a single number is passed", function() {
+    expect(calculator.add("1")).toEqual(1);
+  });
 });
